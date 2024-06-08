92d298a4f50e8f06729f554476964a7ca8fa126f
mahirmm <77577390+mahirmm@users.noreply.github.com>
2024-06-09 00:42:04 +0100

    Create bash4crontab.sh

---
+++ bash4crontab.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sudo python3 whitelist.py --dir /path/to/etc/pihole --docker
+sudo python3 /home/medcroft/shared/MedcroftAthenaAppData/pihole/whitelist-pihole/scripts/whitelist.py --dir /home/medcroft/shared/MedcroftAthenaAppData/pihole/pihole/ --docker
